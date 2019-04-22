d69014d4d9bdea594234fe6b9cf14999a2e93f77
kaysond <kaysond@hotmail.com>
2019-04-22 13:15:25 -0700

    added ipfs pinning script

---
+++ scripts/ipfs_pin.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+set -e
+SHWHASH=QmaitnsR9myMoiJBjqRPY6LuHohTcDnLBDvGWdHZpgfNsj
+IPADDR=$(dig +short selfhosted.wiki)
+echo "Connectecting to selfhosted.wiki ipfs peer"
+ipfs swarm connect /ip4/"$IPADDR"/tcp/4001/ipfs/$SHWHASH
+echo "Removing all old pins"
+ipfs pin ls --type recursive | cut -d' ' -f1 | xargs -n1 ipfs pin rm && ipfs repo gc
+echo "Pinning /ipns/selfhosted.wiki"
+ipfs pin add /ipns/selfhosted.wiki
