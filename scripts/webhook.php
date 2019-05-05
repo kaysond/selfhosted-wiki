c6f25235413801c122158c4391142e11ad34c99a
kaysond <kaysond@hotmail.com>
2019-05-04 23:45:36 -0700

    update webhook

--- scripts/webhook.php
+++ scripts/webhook.php
@@ -3,18 +3,25 @@
 define('SECRET','');
 $postBody = file_get_contents('php://input');
 $mac  = 'sha1=' . hash_hmac( 'sha1' , $postBody , SECRET);
-
+$output = '';
 if ( $mac !== $_SERVER['HTTP_X_HUB_SIGNATURE'] ) {
-        $output = "MAC match failed\n\nRequest:{$_SERVER["HTTP_X_HUB_SIGNATURE"]}\n\nMessage:$mac\n\n";
-        $output .= "BODY: $postBody";
+        $output .= "MAC match failed\n\nRequest:{$_SERVER["HTTP_X_HUB_SIGNATURE"]}\n\nMessage:$mac\n\n";
+        $output .= "BODY: $postBody \n";
 }
 else {
-        $output = "MAC verified\n";
-        $out = array();
-        exec('/usr/local/bin/update-selfhosted-wiki 2>&1', $out, $exitcode);
-        $output .= "Exit: $exitcode\n";
-        $output .= implode("\n", $out) . "\n";
+        $output = "MAC Verified\n";
+        if ( strstr($_REQUEST["ref"], "master") ) {
+                $out = array();
+                exec('/usr/local/bin/update-selfhosted-wiki 2>&1', $out, $exitcode);
+                $output .= "Exit: $exitcode\n";
+                $output .= implode("\n", $out) . "\n";
+        }
+        else {
+                $output .= "Not master branch\n";
+        }
 }
 
 file_put_contents("/var/log/webhook.log", $output);
+
 ?>
+
