b9efdbb4f0f0e0f35a033728a0daaa5e413046c1
kaysond <kaysond@hotmail.com>
2019-05-04 23:58:38 -0700

    fixed webhook script

--- scripts/webhook.php
+++ scripts/webhook.php
@@ -10,18 +10,24 @@ if ( $mac !== $_SERVER['HTTP_X_HUB_SIGNATURE'] ) {
 }
 else {
         $output = "MAC Verified\n";
-        if ( strstr($_REQUEST["ref"], "master") ) {
-                $out = array();
-                exec('/usr/local/bin/update-selfhosted-wiki 2>&1', $out, $exitcode);
-                $output .= "Exit: $exitcode\n";
-                $output .= implode("\n", $out) . "\n";
+        if ( !isset($_REQUEST["payload"]) ) {
+                $output .= "NO PAYLOAD \n";
         }
         else {
-                $output .= "Not master branch\n";
+                $payload = json_decode(urldecode($_REQUEST["payload"]), true);
+                if ( strstr($payload["ref"], "master") ) {
+                        $out = array();
+                        exec('/usr/local/bin/update-selfhosted-wiki 2>&1', $out, $exitcode);
+                        $output .= "Exit: $exitcode\n";
+                        $output .= implode("\n", $out) . "\n";
+                }
+                else {
+                        $output .= "Not master branch\n";
+                        $output .= "BODY: $postBody \n";
+                }
         }
 }
 
 file_put_contents("/var/log/webhook.log", $output);
-
 ?>
 
